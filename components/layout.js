315a4eaeea60dc8652ba101423b6675fe4ddcd4f
Jelle Z <public@jellezwie.rs>
2023-04-18 15:49:39 +0200

    Add additional icons throughout interface

--- components/layout.js
+++ components/layout.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import Link from 'next/link'
 import styles from "./layout.module.css"
 
-import { MdMenu, MdHome, MdFeed, MdMail, MdPublic, MdPinDrop, MdBorderColor, MdRateReview, MdMovie, MdGamepad, MdBook } from 'react-icons/md';
+import { MdMenu, MdHome, MdFeed, MdMail, MdPublic, MdPinDrop, MdBorderColor, MdRateReview, MdMovie, MdGamepad, MdBook, MdMessage, MdPolicy } from 'react-icons/md';
 import { RiTreasureMapFill } from 'react-icons/ri';
+import { SiGithub } from 'react-icons/si';
+
 
 export default function Layout({ children }) {
 
@@ -12,39 +14,39 @@ export default function Layout({ children }) {
   return (
     <div className={styles.layout}>
       <div className={styles.header}>
-        <span className={styles["header__item"]} style={{cursor: "pointer"}} onClick={() => setNavigationVisibility(!navigationVisibility)}>Menu</span>
+        <span className={styles["header__item"]} style={{ cursor: "pointer" }} onClick={() => setNavigationVisibility(!navigationVisibility)}>Menu</span>
         <span className={styles["header__item"]}>Bluefi.re</span>
         <div className={`${styles["header__item"]} ${styles.dropdown}`}>
           <span>More</span>
           <div className={styles["dropdown__content"]}>
-            <Link href="/contact">Contact</Link>
-            <Link href="/legal">Legal</Link>
-            <Link target="_blank" href="https://github.com/jelle619/bluefi.re">GitHub</Link>
+            <span><MdMessage /> <Link href="/contact">Contact</Link></span>
+            <span><MdPolicy /> <Link href="/legal">Legal</Link></span>
+            <span><SiGithub /> <Link target="_blank" href="https://github.com/jelle619/bluefi.re">GitHub</Link></span>
           </div>
         </div>
       </div>
       <div className={[styles.navigation, navigationVisibility ? styles["navigation--visible"] : styles["navigation--invisible"]].join(' ')}>
         <nav>
           <ul className={styles["navigation__list"]}>
-            <li><MdHome/> <Link onClick={() => setNavigationVisibility(false)} href="/">Home</Link></li>
-            <li><MdFeed/> <Link onClick={() => setNavigationVisibility(false)} href="/posts">Posts</Link></li>
-            <li><MdMail/> <Link onClick={() => setNavigationVisibility(false)} href="/postage">Postage</Link></li>
+            <li><MdHome /> <Link onClick={() => setNavigationVisibility(false)} href="/">Home</Link></li>
+            <li><MdFeed /> <Link onClick={() => setNavigationVisibility(false)} href="/posts">Posts</Link></li>
+            <li><MdMail /> <Link onClick={() => setNavigationVisibility(false)} href="/postage">Postage</Link></li>
             <li>Travel</li>
             <ul>
-              <li><MdPublic/> <Link onClick={() => setNavigationVisibility(false)} href="/travel/countries">Countries</Link></li>
-              <li><MdPinDrop/> <Link onClick={() => setNavigationVisibility(false)} href="/travel/places">Places</Link></li>
-              <li><RiTreasureMapFill/> <Link onClick={() => setNavigationVisibility(false)} href="/travel/geocaching">Geocaching</Link></li>
+              <li><MdPublic /> <Link onClick={() => setNavigationVisibility(false)} href="/travel/countries">Countries</Link></li>
+              <li><MdPinDrop /> <Link onClick={() => setNavigationVisibility(false)} href="/travel/places">Places</Link></li>
+              <li><RiTreasureMapFill /> <Link onClick={() => setNavigationVisibility(false)} href="/travel/geocaching">Geocaching</Link></li>
             </ul>
             <li>Art</li>
             <ul>
-              <li><MdRateReview/> <Link onClick={() => setNavigationVisibility(false)} href="/art/commissions">Commissions</Link></li>
-              <li><MdBorderColor/> <Link onClick={() => setNavigationVisibility(false)} href="/art/personal">Personal</Link></li>
+              <li><MdRateReview /> <Link onClick={() => setNavigationVisibility(false)} href="/art/commissions">Commissions</Link></li>
+              <li><MdBorderColor /> <Link onClick={() => setNavigationVisibility(false)} href="/art/personal">Personal</Link></li>
             </ul>
             <li>Hoard</li>
             <ul>
-              <li><MdMovie/> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/movies">Movies</Link></li>
-              <li><MdGamepad/> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/games">Games</Link></li>
-              <li><MdBook/> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/books">Books</Link></li>
+              <li><MdMovie /> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/movies">Movies</Link></li>
+              <li><MdGamepad /> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/games">Games</Link></li>
+              <li><MdBook /> <Link onClick={() => setNavigationVisibility(false)} href="/hoard/books">Books</Link></li>
             </ul>
           </ul>
         </nav>
--- pages/hoard/games.js
+++ pages/hoard/games.js
@@ -1,13 +1,15 @@
 import Link from "next/link"
 import Text from '/components/text.js';
+import { SiSteam, SiGogdotcom, SiXbox } from 'react-icons/si';
 
 export default function Games() {
   return (
     <Text>
       <h1>Games</h1>
       <ul>
-        <li><Link href="https://steamcommunity.com/id/jelle619/games/">Steam</Link></li>
-        <li><Link href="https://www.gog.com/u/jelle619/games">GOG.com</Link></li>
+        <li><SiSteam/> <Link target="_blank" href="https://steamcommunity.com/id/jelle619/games/">Steam</Link></li>
+        <li><SiGogdotcom/> <Link target="_blank" href="https://www.gog.com/u/jelle619/games">GOG.com</Link></li>
+        <li><SiXbox/> <Link target="_blank" href="https://account.xbox.com/profile?gamertag=jelle619">Xbox</Link></li>
       </ul>
     </Text>
   )
